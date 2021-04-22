e4452aaec24afae1fa55e50bdd2e0482b148146e
Ken Snyder <kendsnyder@gmail.com>
2021-04-22 11:49:59 -0600

    [master] update useHook.stories.js to have correct "component" config

--- templates/hook/useHook.stories.js
+++ templates/hook/useHook.stories.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import __name__ from './__name__.js';
+// import any items you need; including a Component.stories.css if applicable
+// TODO: delete this comment block
 
 export default {
 	title: 'Sharpr/hooks/__name__()',
-	component: Example,
+	component: () => {},
 };
 
+// The name any exported functions become a label under Sharpr/components/__name__.
+// "Example" is fine if you have only one example
+// TODO: delete this comment block
 export function Example() {
 	const { res1, res2 } = __name__(arg1, arg2);
 	return (
@@ -15,3 +20,9 @@ export function Example() {
 		</div>
 	);
 }
+
+// Initial values to show on the "try it" table
+Example.args = {
+	prop1: 'a',
+	prop2: 2,
+};
