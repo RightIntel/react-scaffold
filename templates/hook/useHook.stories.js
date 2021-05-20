7f181a7f96093d99ec1361ac5f56cd87bf83b844
Ken Snyder <kendsnyder@gmail.com>
2021-05-20 17:34:52 -0600

    [master] update scaffold for useHook that uses __docgenInfo prop

--- templates/hook/useHook.stories.js
+++ templates/hook/useHook.stories.js
@@ -5,14 +5,14 @@ import __name__ from './__name__.js';
 
 export default {
 	title: 'Sharpr/hooks/__name__()',
-	component: () => {},
+	component: __name__,
 };
 
-// The name any exported functions become a label under Sharpr/components/__name__.
+// The name any exported functions become a label under Sharpr/hooks/__name__.
 // "Example" is fine if you have only one example
 // TODO: delete this comment block
-export function Example() {
-	const { res1, res2 } = __name__(arg1, arg2);
+export function Example(args) {
+	const { res1, res2 } = __name__(args.prop1, args.prop2);
 	return (
 		<div className="Component MyComponent">
 			<div>TODO: Explain hook here</div>
@@ -25,4 +25,25 @@ export function Example() {
 Example.args = {
 	prop1: 'a',
 	prop2: 2,
+};
+
+// Storybook is not able to extract the hooks code from "Example"
+// Please copy the source of the Example function above so that
+// the code will show behind the "Show code" button.
+Example.parameters = {
+	docs: {
+		source: {
+			code: `
+export function Example(args) {
+	const { res1, res2 } = __name__(args.prop1, args.prop2);
+	return (
+		<div className="Component MyComponent">
+			<div>TODO: Explain hook here</div>
+			<div className={res1}>{res2}</div>
+		</div>
+	);
+}
+`,
+		},
+	},
 };
